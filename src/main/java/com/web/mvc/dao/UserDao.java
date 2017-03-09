b851b6d30a48a4f11f65e36e11701f96c65a8e44
xieyuhui <xieyuhui728@qq.com>
2017-03-09 20:28:09 +0800

    add mybatis config

--- src/main/java/com/web/mvc/dao/UserDao.java
+++ src/main/java/com/web/mvc/dao/UserDao.java
@@ -1,7 +1,10 @@
 package com.web.mvc.dao;
 
+import com.web.mvc.admin.model.User;
+
 /**
  * Created by xieyuhui on 2017/2/16.
  */
 public interface UserDao {
+    User getUser(User user);
 }
--- src/main/java/com/web/mvc/dao/impl/UserDaoImpl.java
+++
@@ -1,9 +0,0 @@
-package com.web.mvc.dao.impl;
-
-import com.web.mvc.dao.UserDao;
-
-/**
- * Created by xieyuhui on 2017/2/16.
- */
-public class UserDaoImpl implements UserDao {
-}
